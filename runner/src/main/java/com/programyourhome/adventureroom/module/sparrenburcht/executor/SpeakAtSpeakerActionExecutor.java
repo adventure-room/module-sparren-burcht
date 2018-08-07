2de4c7cbc9f6d652e63a8c4240e10539782e8222
Erik Mulder <ewjmulder@yahoo.com>
2018-08-07 23:04:35 +0200

    Refactor to match new Immerse API

--- runner/src/main/java/com/programyourhome/adventureroom/module/sparrenburcht/executor/SpeakAtSpeakerActionExecutor.java
+++ runner/src/main/java/com/programyourhome/adventureroom/module/sparrenburcht/executor/SpeakAtSpeakerActionExecutor.java
@@ -1,35 +1,24 @@
 package com.programyourhome.adventureroom.module.sparrenburcht.executor;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
-import java.util.Random;
 
-import com.programyourhome.adventureroom.module.amazonpolly.service.model.PollyResult;
+import javax.sound.sampled.AudioInputStream;
+
+import com.programyourhome.adventureroom.model.toolbox.DataStream;
 import com.programyourhome.adventureroom.module.sparrenburcht.model.SpeakAtSpeakerAction;
-import com.programyourhome.adventureroom.module.sparrenburcht.util.WaveUtil;
+import com.programyourhome.immerse.domain.format.ImmerseAudioFormat;
 import com.programyourhome.iotadventure.runner.context.ExecutionContext;
 
 public class SpeakAtSpeakerActionExecutor extends AbstractSparrenBurchtExecutor<SpeakAtSpeakerAction> {
 
     @Override
     public void execute(SpeakAtSpeakerAction action, ExecutionContext context) {
-        PollyResult pollyResult = this.getAmazonPolly(context).synthesizeText(action.character.voiceId, action.text);
-        File cacheFile = this.cacheLocally(pollyResult);
-        this.getImmerse(context).playAtSpeakers("http://localhost:19161/audio/" +
-                cacheFile.getName().substring(0, cacheFile.getName().length() - 4), Arrays.asList(action.speaker), false, true);
-    }
-
-    private File cacheLocally(PollyResult pollyResult) {
-        try {
-            // TODO: make configurable
-            File cacheFile = new File("/home/emulder/Downloads/audio/audio" + new Random().nextLong() + ".wav");
-            WaveUtil.writeWave(new FileOutputStream(cacheFile), pollyResult.audioFormat, pollyResult.inputStream);
-            return cacheFile;
-        } catch (IOException e) {
-            throw new IllegalStateException("IOException during local caching", e);
-        }
+        AudioInputStream audioInputStream = this.getAmazonPolly(context).synthesizeText(action.character.voiceId, action.text);
+        ImmerseAudioFormat format = ImmerseAudioFormat.fromJavaAudioFormat(audioInputStream.getFormat());
+        DataStream dataStream = new DataStream(audioInputStream, "audio/pcm");
+        URL url = this.getAmazonPollyModule(context).getToolbox().getDataStreamToUrl().exposeDataStream(dataStream);
+        this.getImmerse(context).playAtSpeakers(url.toString(), format, Arrays.asList(action.speaker), false, true);
     }
 
 }
