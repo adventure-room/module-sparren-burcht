0912c3b23f9620267ed2547aa742ef410b88fdf1
Erik Mulder <ewjmulder@yahoo.com>
2018-08-27 23:08:02 +0200

    Work in progress: regex action refactoring

--- dsl/src/main/java/com/programyourhome/adventureroom/module/sparrenburcht/dsl/converters/SpeakAtSpeakersActionConverter.java
+++ dsl/src/main/java/com/programyourhome/adventureroom/module/sparrenburcht/dsl/converters/SpeakAtSpeakersActionConverter.java
@@ -1,18 +1,26 @@
 package com.programyourhome.adventureroom.module.sparrenburcht.dsl.converters;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+
 import com.programyourhome.adventureroom.dsl.regex.MatchResult;
-import com.programyourhome.adventureroom.dsl.regex.RegexActionConverter;
 import com.programyourhome.adventureroom.model.Adventure;
 import com.programyourhome.adventureroom.module.immerse.model.SpeakerExternalResource;
 import com.programyourhome.adventureroom.module.sparrenburcht.model.SpeakAtSpeakersAction;
 
 import one.util.streamex.StreamEx;
 
-public class SpeakAtSpeakersActionConverter implements RegexActionConverter<SpeakAtSpeakersAction> {
+public class SpeakAtSpeakersActionConverter extends AbstractSparrenBurchtActionConverter<SpeakAtSpeakersAction> {
 
     @Override
-    public String getRegexLine() {
-        return CHARACTER_ID + " says " + TEXT + " at " + RESOURCE_ID_LIST;
+    public Map<String, String> getRegexMap() {
+        return this.createRegexes(
+                "single", CHARACTER_ID + " says " + TEXT + " at speaker " + SPEAKER_ID,
+                "multiple", CHARACTER_ID + " says " + TEXT + " at speakers " + SPEAKER_IDS,
+                "all", CHARACTER_ID + " says " + TEXT + " at all speakers",
+                "default", CHARACTER_ID + " says " + TEXT);
     }
 
     @Override
@@ -20,10 +28,18 @@ public class SpeakAtSpeakersActionConverter implements RegexActionConverter<Spea
         SpeakAtSpeakersAction action = new SpeakAtSpeakersAction();
         action.character = adventure.getCharacter(matchResult.getValue(CHARACTER_ID));
         action.text = matchResult.getValue(TEXT);
-        String[] resourceIds = matchResult.getValue(RESOURCE_ID_LIST).split(",");
-        action.speakers = StreamEx.of(resourceIds)
-                .map(id -> adventure.getResource(SpeakerExternalResource.class, id).getWrappedObject())
-                .toSet();
+        Collection<String> speakerIds = new ArrayList<>();
+        if (matchResult.getRegexName().equals("single")) {
+            speakerIds = Arrays.asList(matchResult.getValue(SPEAKER_ID));
+        } else if (matchResult.getRegexName().equals("multiple")) {
+            speakerIds = Arrays.asList(matchResult.getValue(SPEAKER_IDS).split(","));
+        } else if (matchResult.getRegexName().equals("all")) {
+            speakerIds = StreamEx.of(adventure.getExternalResources(SpeakerExternalResource.class))
+                    .map(speaker -> "" + speaker.getId())
+                    .toSet();
+        }
+        action.defaultSpeakers = matchResult.getRegexName().equals("default");
+        action.speakerIds = speakerIds;
         return action;
     }
 
--- model/src/main/java/com/programyourhome/adventureroom/module/sparrenburcht/model/SpeakAtSpeakersAction.java
+++ model/src/main/java/com/programyourhome/adventureroom/module/sparrenburcht/model/SpeakAtSpeakersAction.java
@@ -1,15 +1,16 @@
 package com.programyourhome.adventureroom.module.sparrenburcht.model;
 
-import java.util.Set;
+import java.util.Collection;
 
 import com.programyourhome.adventureroom.model.script.action.Action;
 import com.programyourhome.adventureroom.module.amazonpolly.model.characters.PollyCharacter;
-import com.programyourhome.immerse.domain.speakers.Speaker;
 
 public class SpeakAtSpeakersAction implements Action {
 
     public PollyCharacter character;
     public String text;
-    public Set<Speaker> speakers;
+    public boolean defaultSpeakers;
+    // If defaultSpeakers is false, this must be filled with the specific speaker ids
+    public Collection<String> speakerIds;
 
 }
